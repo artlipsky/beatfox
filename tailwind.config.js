618e2e2ccb7dff993f836a0eef599196c7838d91
Arthur Lipsky <lipskyarthur@gmail.com>
2025-09-02 00:06:39 +0300

    [ADD] Astro Layout Structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-const {heroui} = require("@heroui/react");
+import {heroui} from "@heroui/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
