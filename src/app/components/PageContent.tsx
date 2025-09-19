9bb5fb477cb029d78b7b21995db105f77502882d
Arthur Lipsky <lipskyarthur@gmail.com>
2025-09-19 22:41:14 +0300

    [ADD] Chip and Skeletons

--- src/app/components/PageContent.tsx
+++ src/app/components/PageContent.tsx
@@ -1,10 +1,26 @@
-import { Card, CardBody } from '@heroui/react';
+import { Card, CardBody, Chip, Button } from '@heroui/react';
+import { PencilIcon } from '@heroicons/react/16/solid';
 
 export default function PageContent() {
   return (
     <Card className="p-6" radius="sm" shadow="sm" fullWidth>
       <CardBody>
-        <h1 className="font-semibold text-2xl">Page Title</h1>
+        <div className="flex justify-between items-center gap-2 w-full">
+          <h1 className="items-center font-semibold text-2xl">
+            Fence Installation #1198367{' '}
+            <Chip color="warning" size="sm" variant="solid" className="inline-flex bottom-1 relative">
+              Preparing Project
+            </Chip>
+          </h1>
+          <Button
+            color="default"
+            size="sm"
+            variant="bordered"
+            startContent={<PencilIcon className="size-4" />}
+          >
+            Change Order
+          </Button>
+        </div>
       </CardBody>
     </Card>
   );
--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -6,6 +6,7 @@ import {
   Divider,
   Button,
   Skeleton,
+  Chip,
 } from '@heroui/react';
 import { GiftIcon } from '@heroicons/react/16/solid';
 import { primaryMenuItems, menuItems } from '../constants/menuItems';
@@ -27,24 +28,50 @@ export default function SideBar() {
             {primaryMenuItems.map(item => (
               <ListboxItem
                 key={item.key}
-                startContent={<item.icon className="opacity-50 size-4" />}
+                startContent={
+                  <Skeleton isLoaded={isLoaded} className="rounded-full">
+                    <item.icon className="opacity-50 size-4" />
+                  </Skeleton>
+                }
                 href="#"
                 variant="flat"
               >
-                {item.name}
+                <Skeleton isLoaded={isLoaded} className="rounded">
+                  {item.name}
+                </Skeleton>
               </ListboxItem>
             ))}
           </Listbox>
+          <Skeleton isLoaded={isLoaded} className="rounded">
+            <p className="px-2 py-1 font-regular text-gray-500 dark:text-gray-400 text-xs uppercase">
+              Accessing as Arthur Lipsky
+            </p>
+          </Skeleton>
           <Divider />
           <Listbox aria-label="Secondary navigation">
             {menuItems.map(item => (
               <ListboxItem
                 key={item.key}
-                startContent={<item.icon className="opacity-50 size-4" />}
+                startContent={
+                  <Skeleton isLoaded={isLoaded} className="rounded-full">
+                    <item.icon className="opacity-50 size-4" />
+                  </Skeleton>
+                }
                 href="#"
                 variant="flat"
+                endContent={
+                  item.key === 'tasks' ? (
+                    <Skeleton isLoaded={isLoaded} className="rounded-full">
+                      <Chip color="danger" size="sm" variant="solid">
+                        3
+                      </Chip>
+                    </Skeleton>
+                  ) : null
+                }
               >
-                {item.name}
+                <Skeleton isLoaded={isLoaded} className="rounded">
+                  {item.name}
+                </Skeleton>
               </ListboxItem>
             ))}
           </Listbox>
