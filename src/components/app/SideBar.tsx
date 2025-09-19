3a704a3dea675e4a3c66df1f27589713170c8113
Arthur Lipsky <lipskyarthur@gmail.com>
2025-09-19 15:54:10 +0300

    [ADD] Project Details Page

---
+++ src/components/app/SideBar.tsx
@@ -0,0 +1,11 @@
+import { Card, CardBody } from '@heroui/react';
+
+export default function SideBar() {
+  return (
+    <Card radius="sm" shadow="sm" fullWidth>
+      <CardBody>
+        <p>Menu</p>
+      </CardBody>
+    </Card>
+  );
+}
