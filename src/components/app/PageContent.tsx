7cda533122f5fb20d37dc947b9833d8b9d7799e7
Arthur Lipsky <lipskyarthur@gmail.com>
2025-09-19 15:54:10 +0300

    [ADD] Project Details Page

---
+++ src/components/app/PageContent.tsx
@@ -0,0 +1,11 @@
+import { Card, CardBody } from '@heroui/react';
+
+export default function PageContent() {
+  return (
+    <Card radius="sm" shadow="sm" fullWidth>
+      <CardBody>
+        <p>PageTitle</p>
+      </CardBody>
+    </Card>
+  );
+}
